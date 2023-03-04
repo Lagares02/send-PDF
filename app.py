38faf77f28b52faffbb560e33b57c47e5d041c28
unknown <Hlagares@utb.edu.co>
2023-03-04 17:29:04 -0500

    api de crear y enviar pdf con el metodo 2

--- app.py
+++ app.py
@@ -1,69 +1,65 @@
-from flask import Flask, jsonify, request, make_response
-from jinja2 import Environment, FileSystemLoader
-from PyPDF2 import PdfFileReader, PdfWriter
-import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email.mime.application import MIMEApplication
+from flask import Flask, request, send_file
+from flask_mail import Mail, Message
+from flask import render_template
+import pdfkit
+from decimal import Decimal
 
 app = Flask(__name__)
+app.config['MAIL_SERVER']='smtp.office365.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USERNAME'] = 'storebabyprince@outlook.com'
+app.config['MAIL_PASSWORD'] = 'Janina123@'
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
 
-@app.route('/pdf', methods=['POST'])
+mail = Mail(app)
+
+@app.route('/generate_pdf', methods=['POST'])
 def generate_pdf():
-    # Obtener los datos del JSON de la solicitud
     data = request.get_json()
+    numero_factura = data['numero_factura']
+    fecha_emision = data['fecha_emision']
+    nombre_cliente = data['nombre_cliente']
+    email_cliente = data['email_cliente']
+    telefono_cliente = data['telefono_cliente']
+    direccion_cliente = data['direccion_cliente']
+    nombre_admin = data['nombre_admin']
+    email_admin = data['email_admin']
+    telefono_admin = data['telefono_admin']
+    nombre_articulo1 = data['nombre_articulo1']
+    descripcion_articulo1 = data['descripcion_articulo1']
+    cant1 = Decimal(data['cant1'])
+    precio_unit = Decimal(data['precio_unit'])
+    precio_total = Decimal(data['precio_total'])
+    subtotal = Decimal(data['subtotal'])
+    impuestos = Decimal(data['impuestos'])
+    total_pagar = Decimal(data['total_pagar'])
 
-    # Obtener el nombre del archivo y la ruta del template
-    filename = data['filename']
-    template_path = data['template_path']
+    # Renderizar el template HTML con los datos del usuario
+    html = render_template('form.html', numero_factura=numero_factura, fecha_emision=fecha_emision,
+                           nombre_cliente=nombre_cliente, email_cliente=email_cliente,
+                           telefono_cliente=telefono_cliente, direccion_cliente=direccion_cliente,
+                           nombre_admin=nombre_admin, email_admin=email_admin, telefono_admin=telefono_admin,
+                           nombre_articulo1=nombre_articulo1, descripcion_articulo1=descripcion_articulo1,
+                           cant1="${:.2f}".format(cant1), precio_unit="${:.2f}".format(precio_unit),
+                           precio_total="${:.2f}".format(precio_total),
+                           subtotal="${:.2f}".format(subtotal),
+                           impuestos="${:.2f}".format(impuestos),
+                           total_pagar="${:.2f}".format(total_pagar))
 
-    # Obtener las variables del JSON de la solicitud
-    variables = data['variables']
+    config = pdfkit.configuration(wkhtmltopdf='C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe')
+    # Genera el archivo PDF utilizando pdfkit y wkhtmltopdf
+    pdf_file = 'invoice.pdf'
+    pdfkit.from_string(html, pdf_file, configuration=config)
 
-    # Cargar el template con Jinja2
-    env = Environment(loader=FileSystemLoader('.'))
-    template = env.get_template(template_path)
+    # Mandar el correo electrónico con el archivo PDF adjunto
+    msg = Message('PDF generado', sender='storebabyprince@outlook.com', recipients=[email_cliente])
+    with app.open_resource(pdf_file) as fp:
+        msg.attach(pdf_file, "application/pdf", fp.read())
+    mail.send(msg)
 
-    # Renderizar el template con las variables
-    rendered_template = template.render(variables)
-
-    # Crear un objeto PDF vacío
-    output_pdf = PdfWriter()
-
-    # Agregar cada página del template renderizado al objeto PDF
-    rendered_template_bytes = rendered_template.encode('utf-8')
-    rendered_template_pdf = PdfFileReader(data=rendered_template_bytes)
-    for page_num in range(rendered_template_pdf.getNumPages()):
-        output_pdf.addPage(rendered_template_pdf.getPage(page_num))
-
-    # Crear la respuesta de la API con el archivo PDF
-    response = make_response(output_pdf)
-    response.headers['Content-Disposition'] = (
-        f'attachment; filename={filename}.pdf')
-    response.headers['Content-Type'] = 'application/pdf'
-
-    # Obtener la dirección de correo electrónico del destinatario del JSON de la solicitud
-    to_email = data['to_email']
-
-    # Crear el mensaje de correo electrónico con el archivo PDF adjunto
-    message = MIMEMultipart()
-    message['From'] = 'tu_correo_electronico@gmail.com'
-    message['To'] = to_email
-    message['Subject'] = 'PDF generado desde Flask'
-
-    # Adjuntar el archivo PDF al mensaje
-    pdf_attachment = MIMEApplication(response.data, _subtype='pdf')
-    pdf_attachment.add_header(
-        'Content-Disposition', f'attachment; filename={filename}.pdf')
-    message.attach(pdf_attachment)
-
-    # Enviar el mensaje de correo electrónico
-    with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
-        smtp.starttls()
-        smtp.login('tu_correo_electronico@gmail.com', 'tu_contraseña')
-        smtp.sendmail('tu_correo_electronico@gmail.com', to_email, message.as_string())
-
-    return 'PDF generado y enviado por correo electrónico'
+    # Enviar el archivo PDF generado al cliente
+    return send_file(pdf_file, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
